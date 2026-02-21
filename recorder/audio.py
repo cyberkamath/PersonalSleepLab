a8a9196d8ea2566b76af2e5abde0914c8b46eff6
cyberkamath <kamath.r@icloud.com>
2026-02-21 12:07:58 +0100

    refactoring code

--- recorder/audio.py
+++ recorder/audio.py
@@ -2,36 +2,59 @@
 
 import sounddevice as sd
 import soundfile as sf
-import datetime as dt
-
-sample_rate = 16000 # Standard rate for recording Human voice or Snoring
-duration = 10 # seconds
-channels = 1
-
-#Initiate Recording
-audio_data, fs = sf.read('assets/recording.mp3')
-sd.play(audio_data,fs)
-sd.wait()
-
-starting_time = str(dt.datetime.now())
-print("Recording Started at ",starting_time)
-
-# Unique name for Each days Recording
-output_file =  "Sleep_sound_" + starting_time[0:10] + ".mp3"
+from datetime import datetime
+from dataclasses import dataclass
+from pathlib import Path
 
 
-#Record starts here
-recorded_audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels, dtype='float32')
-sd.wait()
+@dataclass
+class RecorderConfig:
+    sample_rate: int = 16000 # Standard rate for recording Human voice or Snoring
+    duration: int = 10 # seconds
+    channels: int = 1
+    output_dir: Path = Path("recordings")
+    assets_dir: Path = Path("assets")
+    file_format: str = "wav"
+    recording_started: str = "recording.mp3"
+    recording_finished: str = "completed.mp3"
 
-#Save the Recordings
-sf.write(output_file, recorded_audio, sample_rate,bitrate_mode='CONSTANT',compression_level=0.99)
+def generate_filename(config : RecorderConfig) -> Path :
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return config.output_dir / f"Sleep_sound_{timestamp}.{config.file_format}"
 
-#Finish Recording
-audio_data, fs = sf.read('assets/completed.mp3')
-sd.play(audio_data,fs)
-sd.wait()
+def play_audio(audio_path: Path):
+    try:
+        audio_data, fs = sf.read(audio_path)
+        sd.play(audio_data,fs)
+        sd.wait()
+    except Exception as e:
+        print(f"Could not play audio at {audio_path}: {e} ")
 
-finishing_time = str(dt.datetime.now())
-print("Recording finished at ",finishing_time)
+
+def record_audio(config: RecorderConfig):
+    
+    config.output_dir.mkdir(exist_ok=True)
+
+    starting_time = datetime.now()
+    print(f"Recording Started at {starting_time}")
+    play_audio(config.assets_dir / config.recording_started)
+
+    recorded_audio = sd.rec(
+        int(config.duration * config.sample_rate),
+        samplerate=config.sample_rate,
+        channels=config.channels,
+        dtype='float32'
+    )
+    sd.wait()
+
+    output_file =  generate_filename(config)
+    sf.write(output_file, recorded_audio, config.sample_rate)
+
+    finish_time = datetime.now()
+    print(f"Recording finished at {finish_time}")
+    play_audio(config.assets_dir / config.recording_finished)
+
+if __name__ == "__main__":
+    config = RecorderConfig()
+    record_audio(config)
 
